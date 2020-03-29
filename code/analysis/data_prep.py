fb7e9226585b53c9ca5533645031fc76aa09ae74
Simon <simfont@umich.edu>
2020-03-29 12:08:53 -0400

    new nn logs

--- code/analysis/data_prep.py
+++ code/analysis/data_prep.py
@@ -6,3 +6,4 @@ train, test, features = data.load_train_test_and_feature_list()
 
 print(set(train["movie_id"].unique()) == set(test["movie_id"].unique()))
 print(set(train["user_id"].unique()) == set(test["user_id"].unique()))
+
