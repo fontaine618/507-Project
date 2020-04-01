ff8cb7d7a6224370be343daec02a6783d6b2bb46
Simon <simfont@umich.edu>
2020-03-31 21:36:49 -0400

    new nn logs

--- code/analysis/nn_embed.py
+++ code/analysis/nn_embed.py
@@ -7,17 +7,19 @@ train, test, features = data.load_train_test_and_feature_list()
 # Train NN
 for type in ["Regressor", "RegressorRelu6", "RegressorSigmoid", "Ordinal", "Classifier"]:
     for layers in [
-        [(256, "relu"), (128, "relu"), (64, "relu")],
-        [(256, "relu"), (64, "relu"), (64, "relu"), (64, "relu")],
+        [(512, "relu"), (64, "relu")],
+        [(1024, "relu"), (64, "relu")],
+        [(1024, "relu"), (128, "relu")],
+        [(1024, "relu"), (256, "relu"), (64, "relu")],
     ]:
-        for n_embed_users in [64, 128]:
+        for n_embed_users in [32, 64]:
             for n_embed_movies in [64, 128]:
                 nn = models.NNEmbed(
                     type=type,
                     layers=layers,
                     n_embed_users=n_embed_users,
                     n_embed_movies=n_embed_movies,
-                    use_features=False
+                    use_features=True
                 )
                 nn.add_train_data(train, "rating", features)
                 nn.train()
@@ -33,4 +35,4 @@ pd.set_option('display.width', 1000)
 metrics = pd.read_table("models/log/nn.tsv", index_col="description")
 print(metrics[[id for id in metrics.columns if id.startswith("cv_")]])
 
-metrics["cv_mse"].sort_values()
+metrics["test_mse"].sort_values()
