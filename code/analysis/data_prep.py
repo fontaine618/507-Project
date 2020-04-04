060dc8075b3e8bcede9aea8d2f8f0cd9ee05c664
Simon <simfont@umich.edu>
2020-04-04 14:32:18 -0400

    methodology simon

--- code/analysis/data_prep.py
+++ code/analysis/data_prep.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 train, test, features = data.load_train_test_and_feature_list()
 
+genres = [col for col in train.columns if col.startswith("genre")]
+tags = [col for col in train.columns if col.startswith("tag")]
+
 print(set(train["movie_id"].unique()) == set(test["movie_id"].unique()))
 print(set(train["user_id"].unique()) == set(test["user_id"].unique()))
 
